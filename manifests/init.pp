866c03083ce7ed4b72230e03af274e4f7e33576e
Mike Farrow <contact@mikefarrow.co.uk>
2015-03-26 11:43:54 +0000

    adding server default

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,7 +11,7 @@ class agent {
 	if $::masterfqdn != '' {
 		$cond_masterfqdn = $::masterfqdn
 	} else {
-		$cond_masterfqdn = $::fqdn
+		$cond_masterfqdn = $::server
 	}
 
 	if $agentenvironment != '' {
