7878ded37b12e3e58881779b5c7084b838496d4c
Mike Farrow <contact@mikefarrow.co.uk>
2015-03-26 14:27:41 +0000

    adding stage

---
+++ manifests/hostname_config.pp
@@ -0,0 +1,22 @@
+class hostname_config {
+
+	host { "${hostname}":
+		ensure => absent,
+		before => Host["${cond_agentfqdn}"],
+	}
+
+	host { "${cond_agentfqdn}":
+		ensure       => present,
+		ip           => '127.0.1.1',
+		host_aliases => [ $cond_hostname, 'localhost' ],
+	} ->
+
+	exec { "hostname ${cond_hostname}":
+		unless       => "hostname | grep -xqe '^${cond_hostname}\$'",
+	} ->
+
+	exec { "echo ${cond_hostname} > /etc/hostname":
+		unless       => "cat /etc/hostname | grep -xqe '^${cond_hostname}\$'",
+	}
+
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -30,24 +30,13 @@ class agent {
 	$cond_fqdn_parts = split($cond_agentfqdn, '[.]')
 	$cond_hostname = $cond_fqdn_parts[0]
 
-	host { "${hostname}":
-		ensure => absent,
-		before => Host["${cond_agentfqdn}"],
+	stage { 'first':
+		before => Stage['main'],
 	}
 
-	host { "${cond_agentfqdn}":
-		ensure       => present,
-		ip           => '127.0.1.1',
-		host_aliases => [ $cond_hostname, 'localhost' ],
-	} ->
-
-	exec { "hostname ${cond_hostname}":
-		unless       => "hostname | grep -xqe '^${cond_hostname}\$'",
-	} ->
-
-	exec { "echo ${cond_hostname} > /etc/hostname":
-		unless       => "cat /etc/hostname | grep -xqe '^${cond_hostname}\$'",
-	} ->
+	class { 'agent::hostname_config':
+		stage = 'first',
+	}
 
 	class { 'puppet':
 		environment => $cond_environment,
