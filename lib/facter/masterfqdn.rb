4b33fc772b375f99fa0f1237dcf9c8081d0f7c64
Mike Farrow <contact@mikefarrow.co.uk>
2015-03-26 18:06:50 +0000

    fixed masterfqdn fact

--- lib/facter/masterfqdn.rb
+++ lib/facter/masterfqdn.rb
@@ -1,5 +1,5 @@
 Facter.add("masterfqdn") do
 	setcode do
-		Facter::Core::Execution.exec('[[ -f /etc/puppet/master_conf ]] && cat /etc/puppet/master_conf')
+		Facter::Core::Execution.exec('cat /etc/puppet/master_conf 2> /dev/null')
 	end
 end
