5aef43272f529c29055668a466b832573c0074de
Mike Farrow <contact@mikefarrow.co.uk>
2015-03-31 13:13:23 +0100

    hmm

--- manifests/hostname_config.pp
+++ manifests/hostname_config.pp
@@ -25,11 +25,11 @@ class agent::hostname_config {
 		ensure       => present,
 		ip           => '127.0.1.1',
 		host_aliases => [ $cond_hostname, 'localhost' ],
-	} ->
+	}
 
 	exec { "hostname ${cond_hostname}":
 		unless       => "hostname | grep -xqe '^${cond_hostname}\$'",
-	} ->
+	}
 
 	exec { "echo ${cond_hostname} > /etc/hostname":
 		unless       => "cat /etc/hostname | grep -xqe '^${cond_hostname}\$'",
