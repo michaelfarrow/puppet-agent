27cb6eeb737f514f3d3e5eeaa30a58ce531e2846
Mike Farrow <contact@mikefarrow.co.uk>
2015-03-31 12:55:49 +0100

    adding mac puppet packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,12 +2,24 @@ class agent {
 
 	Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin/' ] }
 
-	exec { "apt-update":
-		command => "/usr/bin/apt-get update && touch /tmp/apt-get-updated",
-		unless  => "test -e /tmp/apt-get-updated",
+	stage { 'first':
+		before => Stage['main'],
 	}
 
-	Exec["apt-update"] -> Package <| |>
+	if $::osfamily == 'Debian' {
+
+		exec { "apt-update":
+			command => "/usr/bin/apt-get update && touch /tmp/apt-get-updated",
+			unless  => "test -e /tmp/apt-get-updated",
+		}
+
+		Exec["apt-update"] -> Package <| |>
+
+		class { 'agent::hostname_config':
+			stage => 'first',
+		}
+
+	}
 
 	if $::agentfqdn != '' {
 		$cond_agentfqdn = $::agentfqdn
@@ -21,39 +33,52 @@ class agent {
 		$cond_environment = $environment
 	}
 
-	stage { 'first':
-		before => Stage['main'],
-	}
-
-	class { 'agent::hostname_config':
-		stage => 'first',
-	}
-
 	if $::masterfqdn != '' {
 
-		class { 'puppet':
-			environment => $cond_environment,
-			report      => true,
-			pluginsync  => true,
-			agent       => 'running',
-			server      => $::masterfqdn
-		} ->
+		if $::osfamily == 'Darwin' {
 
-		exec { "storing master config":
-			command => "echo '${::masterfqdn}' > /etc/puppet/master_conf",
-			unless  => "test -e /etc/puppet/master_conf",
+			package { 'mac_puppet':
+				ensure   => installed,
+				provider => pkgdmg,
+				source   => 'http://downloads.puppetlabs.com/mac/facter-2.4.1.dmg',
+			}
+
+			package { 'mac_facter':
+				ensure   => installed,
+				provider => pkgdmg,
+				source   => 'http://downloads.puppetlabs.com/mac/puppet-3.7.4.dmg',
+			}
+
 		}
 
-		concat::fragment{'puppet_conf_agent_splay':
-			target  => 'puppet_conf',
-			content => "  splay = true\n",
-			order   => '21',
-		}
+		if $::osfamily == 'Debian' {
 
-		concat::fragment{'puppet_conf_agent_certname':
-			target  => 'puppet_conf',
-			content => "  certname = ${cond_agentfqdn}\n",
-			order   => '22',
+			class { 'puppet':
+				environment => $cond_environment,
+				report      => true,
+				pluginsync  => true,
+				agent       => 'running',
+				server      => $::masterfqdn,
+				before      => Exec['storing master config']
+			}
+
+			exec { "storing master config":
+				command => "echo '${::masterfqdn}' > /etc/puppet/master_conf",
+				unless  => "test -e /etc/puppet/master_conf",
+			}
+
+			concat::fragment{'puppet_conf_agent_splay':
+				target  => 'puppet_conf',
+				content => "  splay = true\n",
+				order   => '21',
+			}
+
+			concat::fragment{'puppet_conf_agent_certname':
+				target  => 'puppet_conf',
+				content => "  certname = ${cond_agentfqdn}\n",
+				order   => '22',
+			}
+
 		}
 
 	}
