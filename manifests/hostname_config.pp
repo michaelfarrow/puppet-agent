6acbea8fc6e209847b684afcbe37a917c2547092
Mike Farrow <contact@mikefarrow.co.uk>
2015-03-31 21:22:47 +0100

    reverting

--- manifests/hostname_config.pp
+++ manifests/hostname_config.pp
@@ -2,34 +2,30 @@ class agent::hostname_config {
 
 	if $::agentfqdn != '' {
 		$cond_agentfqdn = $::agentfqdn
-		notify {"does not equal blank":}
 	} else {
 		$cond_agentfqdn = $::fqdn
-		notify {"is blank":}
 	}
-
-	notify {"cond_agentfqdn: ${cond_agentfqdn}":}
 
 	$cond_fqdn_parts = split($cond_agentfqdn, '[.]')
 	$cond_hostname = $cond_fqdn_parts[0]
 
-	# host { "${hostname}":
-	# 	ensure => absent,
-	# 	before => Host["${cond_agentfqdn}"],
-	# }
+	host { "${hostname}":
+		ensure => absent,
+		before => Host["${cond_agentfqdn}"],
+	}
 
-	# host { "${cond_agentfqdn}":
-	# 	ensure       => present,
-	# 	ip           => '127.0.1.1',
-	# 	host_aliases => [ $cond_hostname, 'localhost' ],
-	# }
+	host { "${cond_agentfqdn}":
+		ensure       => present,
+		ip           => '127.0.1.1',
+		host_aliases => [ $cond_hostname, 'localhost' ],
+	}
 
-	# exec { "hostname ${cond_hostname}":
-	# 	unless       => "hostname | grep -xqe '^${cond_hostname}\$'",
-	# }
+	exec { "hostname ${cond_hostname}":
+		unless       => "hostname | grep -xqe '^${cond_hostname}\$'",
+	}
 
-	# exec { "echo ${cond_hostname} > /etc/hostname":
-	# 	unless       => "cat /etc/hostname | grep -xqe '^${cond_hostname}\$'",
-	# }
+	exec { "echo ${cond_hostname} > /etc/hostname":
+		unless       => "cat /etc/hostname | grep -xqe '^${cond_hostname}\$'",
+	}
 
 }
