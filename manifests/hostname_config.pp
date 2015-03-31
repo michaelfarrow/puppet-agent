cedfed3496e732176522d309b632f1d90213ce44
Mike Farrow <contact@mikefarrow.co.uk>
2015-03-31 21:17:16 +0100

    removing hostname bits for now

--- manifests/hostname_config.pp
+++ manifests/hostname_config.pp
@@ -13,23 +13,23 @@ class agent::hostname_config {
 	$cond_fqdn_parts = split($cond_agentfqdn, '[.]')
 	$cond_hostname = $cond_fqdn_parts[0]
 
-	host { "${hostname}":
-		ensure => absent,
-		before => Host["${cond_agentfqdn}"],
-	}
+	# host { "${hostname}":
+	# 	ensure => absent,
+	# 	before => Host["${cond_agentfqdn}"],
+	# }
 
-	host { "${cond_agentfqdn}":
-		ensure       => present,
-		ip           => '127.0.1.1',
-		host_aliases => [ $cond_hostname, 'localhost' ],
-	}
+	# host { "${cond_agentfqdn}":
+	# 	ensure       => present,
+	# 	ip           => '127.0.1.1',
+	# 	host_aliases => [ $cond_hostname, 'localhost' ],
+	# }
 
-	exec { "hostname ${cond_hostname}":
-		unless       => "hostname | grep -xqe '^${cond_hostname}\$'",
-	}
+	# exec { "hostname ${cond_hostname}":
+	# 	unless       => "hostname | grep -xqe '^${cond_hostname}\$'",
+	# }
 
-	exec { "echo ${cond_hostname} > /etc/hostname":
-		unless       => "cat /etc/hostname | grep -xqe '^${cond_hostname}\$'",
-	}
+	# exec { "echo ${cond_hostname} > /etc/hostname":
+	# 	unless       => "cat /etc/hostname | grep -xqe '^${cond_hostname}\$'",
+	# }
 
 }
